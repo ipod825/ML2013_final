655ec841eaec6456bdb65e04215152938d9ebffe
mingo <ipod825@gmail.com>
2013-12-31 01:44:57 +0800

    move cross validation into classifier

--- Classifier.m
+++ Classifier.m
@@ -23,5 +23,31 @@ methods
     
     function calssifyOne(this,f)
     end
+    
+    function Eval=crossvalidation(this,Y,X,fold)
+        n=size(Y,1);
+        leaveNum=floor(n/fold);
+        % inds = uniformArray(n,1,n);
+        inds=1:n;
+        groups=zeros(n,1);
+        group=0;
+        for i=1:n
+            if mod(i,leaveNum)==1 && group<fold
+                group=group+1;
+            end
+            groups(inds(i))=group;
+        end
+
+        Eval=0;
+        for i=1:fold
+            trainInds=find(groups~=i);
+            testInds=find(groups==i);
+            clear cls;
+            this.train(Y(trainInds,1),X(trainInds,:));
+            pred=this.classify(X(testInds,:));
+            Eval=Eval+(size(find(Y(testInds)~=pred))/size(testInds,1));
+        end
+        Eval=Eval/fold;
+    end
 end % methods
 end % classdef
---
+++ crossvalmain.m
@@ -0,0 +1,45 @@
+global categNum normSideLength isTraining featureFname n 
+isTraining=true;
+GLOBALVAR;
+
+if(~exist('cache','var'))
+    cache=zeros(1,2);
+end
+
+if(~cache(1,1))%cache X
+    [Y, X]=readmatrix(featureFname,n,normSideLength,normSideLength);
+    X=full(X);
+    cache(1,1)=true;
+end
+
+FE=2;
+clear fe; %Use this when debugging. When you have modified the class file, you need to reinitial the class instance.
+switch(FE)
+    case 1
+        fe=DEFeatureExtracter(normSideLength,categNum);
+    case 2
+        fe=EigenFeatureExtracter(floor(normSideLength*normSideLength/20),[]);
+end
+if(~isTraining)
+    load 'featureextracter.mat';
+    fe.copy(featureextracter);
+end
+
+if(~cache(1,2))%cache F
+    F=fe.extract(X);
+    if(isTraining)
+        featureextracter=fe.saveobj;
+        save('featureextracter.mat','featureextracter');
+    end
+    cache(1,2)=true;
+end
+
+
+for gamma=[0.01, 0.1 1 10]
+    for C=[0.01, 0.1 1 10]
+        clear cls;
+        cls=SVMClassifier(size(F,2),categNum);
+        Eval=cls.crossvalidation(Y,F,5);
+        [gamma C Eval]
+    end
+end
